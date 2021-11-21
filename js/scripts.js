ad0b352ac5df61f44706eb6e31d9888d906d9009
delcidj22 <jdelcdid23@gmail.com>
2021-11-20 22:37:39 -0800

    working through business logic to get page to push through

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,40 @@
-$(document).ready(function () {
-  $("#userInput").submit(function(event) {
-    event.preventDefault();
-    const number = $(".userInput").val();
-        $("#results").html(roboger(number));
-    });
-  });
+
+// Utility Logic
+function inArray(number){
+  inputArray = [];
+  for (i = 0; i <= number; i++){
+    inputArray.push(i.toString());
+  }
+  return inputArray;
+  }
 
 // business logic
-function roboger(number){
-  finalArray = [];
-  numberArray = numArray(number)
-  numberArray.forEach(function(num){
+function userInput(number){
+  lastArray = [];
+  inputArray = inArray(number)
+  inputArray.forEach(function(user){
+    if (user.includes("3")){
+      lastArray.push(user + "Won't you be my neighbor?")
+    }
+    else (user.includes("2")){
+      lastArray.push(user + "Boop!");
+    }
+    else if (user.includes("1")) {
+      lastArray.push(user, + "Beep!");
+    }
+    else if {
+      lastArray.push(user ++);
+    }
   });
-}  
+  return lastArray;
+}
+
+});
+
+$(document).ready(function(){
+  $("form.userInput").submit(function(event){
+    event.preventDefault();
+    const number = $(".userNumber").val();
+    $("#results").html(userInput(number));
+  });
+});
